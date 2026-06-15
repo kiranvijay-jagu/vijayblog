9c99675313848f5d16b3a29e4c5485124e627313
Kiran Vijay <youremail@example.com>
2026-06-15 10:22:36 +0530

    Add Render host to ALLOWED_HOSTS

--- ownblog/settings.py
+++ ownblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','vijayblog.com','www.vijayblog.com']
+ALLOWED_HOSTS = ['127.0.0.1','vijayblog.onrender.com','vijayblog.com','www.vijayblog.com']
 
 
 # Application definition
